51fdf6ed077f1d88cd6ee4801602f7d0ea1b788a
Krzysztof Bański <krzys0100@wp.pl>
2023-11-30 22:08:25 +0100

    Project init

---
+++ AoC23.cpp
@@ -0,0 +1,17 @@
+﻿#include "AoC23.hpp"
+
+
+int main()
+{
+	constexpr uint32_t day = 0;
+	switch (day) {
+	case 0: {
+		Day0::Part1();
+		Day0::Part2();
+		break;
+	}
+	case 1: {
+
+	}
+	}
+}
---
+++ Days/Day0.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <spdlog/spdlog.h>
+
+namespace Day0 {
+
+	inline void Part1() {
+		spdlog::info("Day0: Part1!");
+	}
+
+	inline void Part2() {
+		spdlog::info("Day0: Part2!");
+	}
+}
